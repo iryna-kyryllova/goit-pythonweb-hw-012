b49aa30fcfb4febfa9b8494451ec0d44da5f0a96
Iryna Kyryllova <keira.kirillova@gmail.com>
2025-02-04 16:19:22 +0100

    first commit

---
+++ src/database/models.py
@@ -0,0 +1,39 @@
+from datetime import datetime, date
+
+from sqlalchemy import Integer, String, func, Column, ForeignKey, Boolean
+from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase, relationship
+from sqlalchemy.sql.sqltypes import DateTime, Date
+
+
+class Base(DeclarativeBase):
+    created_at: Mapped[datetime] = mapped_column(
+        "created_at", DateTime, default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        "updated_at", DateTime, default=func.now(), onupdate=func.now()
+    )
+
+
+class Contact(Base):
+    __tablename__ = "contacts"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), nullable=False)
+    phone_number: Mapped[str] = mapped_column(String(20), nullable=False)
+    birthday: Mapped[date] = mapped_column(Date, nullable=False)
+    additional_data: Mapped[str] = mapped_column(String(200))
+    user_id = Column(
+        "user_id", ForeignKey("users.id", ondelete="CASCADE"), default=None
+    )
+    user = relationship("User", backref="contacts")
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
+    email = Column(String, unique=True)
+    hashed_password = Column(String)
+    created_at = Column(DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
+    confirmed = Column(Boolean, default=False)
