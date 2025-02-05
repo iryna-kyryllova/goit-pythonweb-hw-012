952afef9612f1e9ae50f05cd006892d6ca736404
Iryna Kyryllova <keira.kirillova@gmail.com>
2025-02-05 13:19:56 +0100

    add integrational tests to auth api

--- src/api/auth.py
+++ src/api/auth.py
@@ -6,6 +6,7 @@ from src.services.auth import create_access_token, Hash, get_email_from_token
 from src.services.users import UserService
 from src.services.email import send_email
 from src.database.db import get_db
+from src.conf import messages
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -24,14 +25,14 @@ async def register_user(
     if email_user:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail="Користувач з таким email вже існує",
+            detail=messages.EMAIL_EXISTS,
         )
 
     username_user = await user_service.get_user_by_username(user_data.username)
     if username_user:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail="Користувач з таким іменем вже існує",
+            detail=messages.USERNAME_ALREADY_EXISTS,
         )
     user_data.password = Hash().get_password_hash(user_data.password)
     new_user = await user_service.create_user(user_data)
@@ -51,13 +52,13 @@ async def login_user(
     if not user or not Hash().verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Неправильний логін або пароль",
+            detail=messages.WRONG_LOGIN_OR_PASSWORD,
             headers={"WWW-Authenticate": "Bearer"},
         )
     if not user.confirmed:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Електронна адреса не підтверджена",
+            detail=messages.EMAIL_NOT_CONFIRMED,
         )
     access_token = await create_access_token(data={"sub": user.username})
     return {"access_token": access_token, "token_type": "bearer"}
@@ -70,12 +71,12 @@ async def confirmed_email(token: str, db: Session = Depends(get_db)):
     user = await user_service.get_user_by_email(email)
     if user is None:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Verification error"
+            status_code=status.HTTP_400_BAD_REQUEST, detail=messages.VERIFICATION_ERROR
         )
     if user.confirmed:
-        return {"message": "Ваша електронна пошта вже підтверджена"}
+        return {"message": messages.EMAIL_ALREADY_CONFIRMED}
     await user_service.confirmed_email(email)
-    return {"message": "Електронну пошту підтверджено"}
+    return {"message": messages.EMAIL_CONFIRMED}
 
 
 @router.post("/request_email")
@@ -89,10 +90,10 @@ async def request_email(
     user = await user_service.get_user_by_email(body.email)
 
     if user.confirmed:
-        return {"message": "Ваша електронна пошта вже підтверджена"}
+        return {"message": messages.EMAIL_ALREADY_CONFIRMED}
     if user:
         background_tasks.add_task(
             send_email, user.email, user.username, request.base_url
         )
-    return {"message": "Перевірте свою електронну пошту для підтвердження"}
+    return {"message": messages.CHECK_EMAIL}
 
